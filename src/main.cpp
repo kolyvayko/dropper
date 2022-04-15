7d41962471798f1fe65502b75f99a8371820e3f2
k0xxx <androidk0xxx@gmail.com>
2022-04-15 18:01:00 +0300

    add softservo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include <SoftwareServo.h>
 
-Servo servoControl;              // Define servo control object
-const int servoPin = 0;          // Servo at Arduino digital
-const int servoDefaultAngle = 0; // Servo default angle
-const int servoTargetAngle = 90; // Servo target angle
-const int servoDelay = 500;      // Servo delay time in ms
+SoftwareServo servoControl;       // Define servo control object
+const int servoPin = 5;           // Servo at Arduino digital
+const int servoDefaultAngle = 0;  // Servo default angle
+const int servoTargetAngle = 120; // Servo target angle
+const int servoDelay = 500;       // Servo delay time in ms
 
-const int prPin = A0;       // Photoresistor at Arduino analog pin A0
+const int prPin = 4;        // Photoresistor at Arduino analog pin A0
 int prValue;				        // Store value from photoresistor (0-1023)
-const int prLaunch = 25;    // Value of Photoresistor action
+const int prLaunch = 800;   // Value of Photoresistor action
 
 void setup() {
   // Photoresistor setup
   pinMode(prPin, INPUT);          // Set PhotoResistor pin
-  servoControl.attach(servoPin);  // Ser Servo pin
+  servoControl.attach(servoPin);  // Set Servo pin
 
   //Servo setup
   delay(500); // Delay before reset
@@ -25,7 +25,7 @@ void setup() {
 void loop() {
     prValue = analogRead(prPin); // Read pResistor
 
-  if (prValue > prLaunch){
+  if (prValue < prLaunch){
       servoControl.write(servoTargetAngle);
       delay(servoDelay);
   } else {
