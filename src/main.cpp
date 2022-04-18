0a95c4f87815689a8ea98ab56a14e28d63f06c7d
k0xxx <androidk0xxx@gmail.com>
2022-04-18 12:04:19 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include <SoftwareServo.h>
 
 SoftwareServo servoControl;       // Define servo control object
-const int servoPin = 5;           // Servo at Arduino digital
+const int servoPin = 4;           // Servo at Arduino digital
 const int servoDefaultAngle = 0;  // Servo default angle
 const int servoTargetAngle = 120; // Servo target angle
 const int servoDelay = 500;       // Servo delay time in ms
 
-const int prPin = 4;        // Photoresistor at Arduino analog pin A0
+const int prPin = 5;        // Photoresistor at Arduino analog pin A0
 int prValue;				        // Store value from photoresistor (0-1023)
 const int prLaunch = 800;   // Value of Photoresistor action
 
