b63dd8c760b0d78db331e0e5fdab0b859fd94c6f
k0xxx <androidk0xxx@gmail.com>
2022-04-20 22:38:56 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 // #include <SoftwareServo.h>
-#include <SimpleServo.h>
+// #include <SimpleServo.h>
+#include <SoftRcPulseOut.h> 
+
 
 // SoftwareServo servoControl;       // Define servo control object
-SimpleServo servoControl;
+// SimpleServo servoControl;
+SoftRcPulseOut servoControl;
+
 const int servoPin = 4;           // Servo at Arduino digital
 const int servoDefaultAngle = 0;  // Servo default angle
 const int servoTargetAngle = 90; // Servo target angle
 int servoAngle = 0;
 int servoTmpAngle = 0;
 const int servoDelay = 500;       // Servo delay time in ms
+const int servoRefresh = 20;      // Servo refresh ms
+
 
 const int prPin = 5;        // Photoresistor at Arduino analog pin A0
 int prValue;				        // Store value from photoresistor (0-1023)
@@ -33,16 +39,33 @@ void loop() {
   prValue = analogRead(prPin); // Read pResistor
 
   if (prValue > prLaunch){
-    servoAngle = servoTargetAngle;
+    // servoAngle = servoTargetAngle;
+    digitalWrite(0, HIGH);
+    digitalWrite(1, HIGH);
+
+    for(servoTmpAngle = 0; servoTmpAngle < servoTargetAngle; servoTmpAngle += 1)  // goes from 0 degrees to 180 degrees 
+    {                                  // in steps of 1 degree 
+      servoControl.write(servoTmpAngle);              // tell servo to go to position in variable 'pos' 
+      delay(servoRefresh);        // waits 20ms for refresh period 
+      SoftRcPulseOut::refresh(1);        // generates the servo pulse
+    } 
   } else {
-    servoAngle = servoDefaultAngle;
+    // servoAngle = servoDefaultAngle;
+    digitalWrite(0, LOW);
+    digitalWrite(1, LOW);
+     for(servoTmpAngle = servoTargetAngle; servoTmpAngle>=1; servoTmpAngle-=1)     // goes from 180 degrees to 0 degrees 
+    {                                
+      servoControl.write(servoTmpAngle);              // tell servo to go to position in variable 'pos' 
+      delay(servoRefresh);        // waits 20ms for for refresh period 
+      SoftRcPulseOut::refresh(1);        // generates the servo pulse
+    } 
   }
   
-  servoControl.write(servoAngle);
-  if(servoAngle != servoTmpAngle){
-    delay(servoDelay);
-  }
-  servoTmpAngle = servoAngle;
+  // servoControl.write(servoAngle);
+  // if(servoAngle != servoTmpAngle){
+  //   delay(servoDelay);
+  // }
+  // servoTmpAngle = servoAngle;
 
   // delay(1); // Idle
 }
