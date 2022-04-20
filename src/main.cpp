93f08846721be2165b0c21b637c4be9f3c156c3e
k0xxx <androidk0xxx@gmail.com>
2022-04-20 17:23:04 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ SimpleServo servoControl;
 const int servoPin = 4;           // Servo at Arduino digital
 const int servoDefaultAngle = 0;  // Servo default angle
 const int servoTargetAngle = 120; // Servo target angle
+int servoAngle = 0;
+int servoTmpAngle = 0;
 const int servoDelay = 500;       // Servo delay time in ms
 
 const int prPin = 5;        // Photoresistor at Arduino analog pin A0
@@ -31,18 +33,16 @@ void loop() {
   prValue = analogRead(prPin); // Read pResistor
 
   if (prValue < prLaunch){
-      digitalWrite(0, HIGH);
-	    digitalWrite(1, HIGH);
-
-      servoControl.write(servoTargetAngle);
-      delay(servoDelay);
+    servoAngle = servoTargetAngle;
   } else {
-      digitalWrite(0, LOW);
-	    digitalWrite(1, LOW);
-
-      servoControl.write(servoDefaultAngle);
-      delay(servoDelay);
+    servoAngle = servoDefaultAngle;
   }
+  
+  servoControl.write(servoAngle);
+  if(servoAngle != servoTmpAngle){
+    delay(servoDelay);
+  }
+  servoTmpAngle = servoAngle;
 
   delay(1); // Idle
 }
