a0c9254ffc2ca7118d4ebcdf62836ff4c4c34575
k0xxx <androidk0xxx@gmail.com>
2022-04-26 14:29:05 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,32 @@
 #include <Arduino.h>
+// #include <SoftwareServo.h>
+// #include <SimpleServo.h>
 #include <SoftRcPulseOut.h> 
 
-SoftRcPulseOut servoControl1;
-SoftRcPulseOut servoControl2;
 
-const int servoPin1 = 4;           // Servo at Arduino digital
-const int servoPin2 = 5;           // Servo at Arduino digital
+// SoftwareServo servoControl;    // Define servo control object
+// SimpleServo servoControl;
+SoftRcPulseOut servoControl;
+
+const int servoPin = 4;           // Servo at Arduino digital
 const int servoDefaultAngle = 0;  // Servo default angle
 const int servoTargetAngle = 70;  // Servo target angle
-int servo1Angle = 0;
-int servo2Angle = 0;
+int servoAngle = 0;
 const int servoDelay = 500;       // Servo delay time in ms
 const int servoRefresh = 20;      // Servo refresh ms
 
 
-const int prPin = 3;       // Photoresistor at Arduino analog pin A0
+const int prPin = 3;        // Photoresistor at Arduino analog pin A0
 int prValue;				        // Store value from photoresistor (0-1023)
 const int prLaunch = 700;   // Value of Photoresistor action
-
-int launchStatus = 1;
 
 void setup() {
   // Photoresistor setup
   pinMode(prPin, INPUT);          // Set PhotoResistor pin
-  servoControl1.attach(servoPin1);  // Set Servo pin1
-  servoControl2.attach(servoPin2);  // Set Servo pin2
+  servoControl.attach(servoPin);  // Set Servo pin
 
   //Servo setup
-  servoControl1.write(servoDefaultAngle); // Set servo to default position
-  servoControl2.write(servoDefaultAngle); // Set servo to default position
+  servoControl.write(servoDefaultAngle); // Set servo to default position
   delay(500); // Delay after reset (waiting for position)
 }
 
@@ -36,24 +34,12 @@ void loop() {
   prValue = analogRead(prPin); // Read pResistor
 
   if (prValue > prLaunch){
-    if(launchStatus == 1 && servo1Angle != servoTargetAngle){
-      servo1Angle = servoTargetAngle;
-      launchStatus = 2;
-    }else if(launchStatus == 2 && servo2Angle != servoTargetAngle){
-      servo2Angle = servoTargetAngle;
-      launchStatus = 1;
-    }
+    servoAngle = servoTargetAngle;
   } else {
-    if(servo1Angle != servoDefaultAngle){
-      servo1Angle = servoDefaultAngle;
-    }
-    if(servo2Angle != servoDefaultAngle){
-      servo2Angle = servoDefaultAngle;
-    }
+    servoAngle = servoDefaultAngle;
   }
   
-  servoControl1.write(servo1Angle);   // tell servo1 to go to position in variable 'servoAngle' 
-  servoControl2.write(servo2Angle);   // tell servo2 to go to position in variable 'servoAngle' 
-  delay(servoRefresh);                // waits 20ms for for refresh period 
-  SoftRcPulseOut::refresh(1);         // generates the servo pulse
+  servoControl.write(servoAngle);    // tell servo to go to position in variable 'servoAngle' 
+  delay(servoRefresh);               // waits 20ms for for refresh period 
+  SoftRcPulseOut::refresh(1);        // generates the servo pulse
 }
