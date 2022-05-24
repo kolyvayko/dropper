5713ce0c6ea9cecc8bf4cf2461598627f5b37447
Oleksii Kolyvaiko <kolyvayko@ukr.net>
2022-05-24 20:24:21 +0300

    d1_mini_work_draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,49 @@
 #include <Arduino.h>
-#include <SoftRcPulseOut.h> 
+// #include <SoftRcPulseOut.h> 
+#include <Servo.h>
 
-SoftRcPulseOut servoControl1;
-SoftRcPulseOut servoControl2;
+Servo  servoControl1;
+Servo servoControl2;
 
-const int servoPin1 = 3;           // Servo Left
-const int servoPin2 = 5;           // Servo Right
-const int servo1DefaultAngle = 60;   // Servo Left default angle
-const int servo1TargetAngle = 0;     // Servo Left target angle
-const int servo2DefaultAngle = 0;    // Servo Right default angle
-const int servo2TargetAngle = 60;    // Servo Right target angle
+const int servoPin1 = 5;           // Servo Left
+const int servoPin2 = 4;           // Servo Right
+const int servo1DefaultAngle = 0;   // Servo Left default angle
+const int servo1TargetAngle = 90;     // Servo Left target angle
+const int servo2DefaultAngle = 90;    // Servo Right default angle
+const int servo2TargetAngle = 0;    // Servo Right target angle
 int servo1Angle = 0;
 int servo2Angle = 0;
 const int servoRefresh = 20;      // Servo refresh ms
 
 
-const int prPin = A3;       // Photoresistor at Arduino analog pin A0
+const int prPin = A0;       // Photoresistor at Arduino analog pin A0
 int prValue;				        // Store value from photoresistor (0-1023)
-const int prLaunch = 15;   // Value of Photoresistor action
+const int prLaunch = 30;   // Value of Photoresistor action
+
+// const int load1 = 7;       // Photoresistor at Arduino analog pin A0
+// const int load2 = 8;       // Photoresistor at Arduino analog pin A0
 
 int launchStatus = 1;
 bool actionDone = false;
 
 bool inited = false;
 
+// int i = 0;
+
 void setup() {
   // Photoresistor setup
   pinMode(prPin, INPUT);          // Set PhotoResistor pin
-  // Serial.begin(9600);
+  // pinMode(load1, OUTPUT);          // Set PhotoResistor pin
+  // pinMode(load2, OUTPUT);          // Set PhotoResistor pin
+  Serial.begin(9600);
   // Servo setup
   servoControl1.attach(servoPin1);  // Set Servo pin1
   servoControl2.attach(servoPin2);  // Set Servo pin2
+  
 }
 
 void loop() {
+  // i++;
   if(!inited){
     servo1Angle = servo1DefaultAngle;
     servo2Angle = servo2DefaultAngle;
@@ -41,7 +51,7 @@ void loop() {
   }
 
   prValue = analogRead(prPin); // Read pResistor
-  // Serial.println(prValue);
+  Serial.println(prValue);
   if (prValue < prLaunch){
     if(!actionDone){
       if(launchStatus == 1){
@@ -66,8 +76,21 @@ void loop() {
     actionDone = false;
   }
   
-  servoControl1.write(servo1Angle);   // tell servo1 to go to position in variable 'servoAngle' 
+  servoControl1.write(servo1Angle);   // tell servo1 to go to position in variable 'servoAngle'
+  delay(15); 
   servoControl2.write(servo2Angle);   // tell servo2 to go to position in variable 'servoAngle' 
+  delay(15); 
   delay(servoRefresh);                // waits 20ms for for refresh period 
-  SoftRcPulseOut::refresh(1);         // generates the servo pulse
+  // SoftRcPulseOut::refresh(1);         // generates the servo pulse
+  // delay(200);
+
+  // // if (i>5){
+  //   digitalWrite(load1,HIGH);
+  //   digitalWrite(load2,HIGH);
+  //   delay(100);
+  //   digitalWrite(load1,LOW);
+  //   digitalWrite(load2,LOW);
+  //   i = 0;
+  // // }
+  
 }
