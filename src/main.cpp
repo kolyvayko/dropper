5136182c834ec0caf10f3bf42f217c5a2f3d77e8
Oleksii Kolyvaiko <kolyvayko@ukr.net>
2022-07-31 21:38:30 +0300

    final

--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,32 @@ struct SRVconfig{
   int CurrentAngle;
 };
 
-Servo ServoControl[5];
+Servo ServoControl[3];
 
 //Servo config
-SRVconfig conf[5] ={
-  {0,false, D5,0,180,0},
-  {1,false, D6,0,180,0},
-  {2,false, D7,160,0,160},
-  {3,false, D2,160,0,160},
-  {4,false, D4,0,180,0}
+SRVconfig conf[3] ={
+  {0,false, D6,0,180,0},
+  {1,false, D5,0,180,0},
+  {2,false, D7,160,0,160}
 };     
-SRVconfig SRVEnabled[5];   
+SRVconfig SRVEnabled[3];   
 
 const int servoRefresh = 20;      // Servo refresh ms
 
 //Photoresistor config
 const int prPin = A0;       
 int prValue;				        // Store value from photoresistor (0-1023)
-const int prLaunch = 30;   // Value of Photoresistor action
+const int prLaunch = 14;   // Value of Photoresistor action
 
 //Load key
 const int loadPin = D3;
-
-//Config pins
-int K[3] = {D0,D8,D1};
 
 int launchStatus = 1;
 bool actionDone = false;
 
 bool serviceMode = false;
 
-int SrvCount=5;
+int SrvCount=3;
 int SrvEnabled=0;
 
 void setup() {
@@ -51,37 +46,16 @@ void setup() {
   //Load key setup
   pinMode(loadPin, INPUT_PULLUP);
 
-  // Config switcher 
-  for (int i=0;i<3;i++){
-    pinMode(K[i],INPUT_PULLUP);
-  }
-
-  int K1Value = digitalRead(K[0]);
-  int K2Value = digitalRead(K[1]);
-  int K3Value = digitalRead(K[2]);
-
-
-  if (K1Value==0){
     SRVEnabled[SrvEnabled]=conf[0];
     SRVEnabled[SrvEnabled].Enabled=true;
     SrvEnabled++;
-  }
-  if (K2Value==0){
+
     SRVEnabled[SrvEnabled]=conf[1];
     SRVEnabled[SrvEnabled].Enabled=true;
     SrvEnabled++;
     SRVEnabled[SrvEnabled]=conf[2];
     SRVEnabled[SrvEnabled].Enabled=true;
     SrvEnabled++;
-  }
-  if (K3Value==0){
-    SRVEnabled[SrvEnabled]=conf[3];
-    SRVEnabled[SrvEnabled].Enabled=true;
-    SrvEnabled++;
-    SRVEnabled[SrvEnabled]=conf[4];
-    SRVEnabled[SrvEnabled].Enabled=true;
-    SrvEnabled++;
-  }
 
   // Servo setup
   for (int i=0;i<SrvCount;i++){
@@ -106,7 +80,6 @@ void loop() {
   // Serial.println(prValue);
   if (prValue < prLaunch){
     if(!actionDone){
-      Serial.println(launchStatus);
       SRVEnabled[launchStatus-1].CurrentAngle = SRVEnabled[launchStatus-1].TargetAngle;
       actionDone = true;
     }
@@ -125,10 +98,8 @@ void loop() {
   }
   
   for (int i=0;i<SrvEnabled;i++){
-    // if (SRVEnabled[i].Enabled){
       ServoControl[SRVEnabled[i].Number].write(SRVEnabled[i].CurrentAngle);
       delay(servoRefresh);
-    // }
   }     
 }
 
